4973da7f7f77f5936fa1cd782d2d6f1fde8455ca
sathishs007s <110343722+sathishs007s@users.noreply.github.com>
2023-05-23 06:33:04 +0000

    useRef practice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React,{ useState,createContext  } from 'react';
+import React,{ useState,createContext,useRef } from 'react';
 import './App.css';
 import Button from './Button';
 
 export const AppContext = React.createContext(null);
 
 function App() {
+  const nameRef = useRef()
   const [count,setCount] = useState(0);
   return (
     <AppContext.Provider value={[count, setCount]}>
       <div className="App">
           {count}
           <Button />
+          <form onSubmit={(e)=>{
+            e.preventDefault()
+             console.log(nameRef.current.value)
+             nameRef.current.value=''
+          }}>
+            <input type='text' ref={nameRef}/>
+  
+            <input type="submit"  value='submit'/>
+          </form>
       </div>
     </AppContext.Provider>
 
